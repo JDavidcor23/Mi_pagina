affc56fcfd73153bbc5f986f955e20b875248aa4
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-12-05 16:49:52 -0500

    link

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <li className='li-nav'><a href="#Proyectos" className='links'>Proyectos</a></li>
                 <li className='li-nav'><a href="#servicios" className='links'>Servicios</a></li>
                 <li><a href="#Contactame" className='links'>Contactame</a></li>
-                <li><a href="https://www.docdroid.net/JPJrmWZ/hoja-de-vida-pdf" className='links' target="_blank"><button className="curriculum">Descargar currículum</button></a></li>
+                <li><a href="https://drive.google.com/file/d/1pr5imKJXATOKEVNOtQQZdYufFY-hv7X3/view?usp=sharing" className='links' target="_blank"rel="noreferrer"><button className="curriculum">Descargar currículum</button></a></li>
             </ul>
         </nav>
     );
