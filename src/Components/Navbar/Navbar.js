118a9b836ad9335c164d6ccab85599bcf0be1865
Jorge David Diaz Cordero <72902196+JDavidcor23@users.noreply.github.com>
2022-01-04 00:12:56 -0500

    curriculum

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <li className='li-nav'><a href="#Proyectos" className='links'>Proyectos</a></li>
                 <li className='li-nav'><a href="#servicios" className='links'>Servicios</a></li>
                 <li><a href="#Contactame" className='links'>Contactame</a></li>
-                <li><a href="https://drive.google.com/file/d/1pr5imKJXATOKEVNOtQQZdYufFY-hv7X3/view?usp=sharing" className='links' target="_blank"rel="noreferrer"><button className="curriculum">Descargar currículum</button></a></li>
+                <li><a href="https://drive.google.com/file/d/1gUIb-C-x2NqWtqpSpVf-y_tgGM1GUxlr/view?usp=sharing" className='links' target="_blank"rel="noreferrer"><button className="curriculum">Descargar currículum</button></a></li>
             </ul>
         </nav>
     );
